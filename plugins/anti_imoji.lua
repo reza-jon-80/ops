a1f81133ed24633417b169136223cb73606a0e81
Mohammad Mahdi <mohammad400ebrahimi@yahoo.com>
2016-03-17 18:32:56 +0330

    Update anti_imoji.lua

--- plugins/anti_imoji.lua
+++ plugins/anti_imoji.lua
@@ -12,7 +12,7 @@ local function run(msg, matches)
     end
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
-    if lock_imoji == "yes" then
+    if lock_imoji == "💚" then
         send_large_msg(chat, 'به دلیل ارسال ایموجی فیس اخراج شد')
         chat_del_user(chat, user, ok_cb, true)
     end
