ad9a176d826449a05ceeaea9b75f61b00f7f6441
Mohammad Mahdi <mohammad400ebrahimi@yahoo.com>
2016-03-17 18:26:09 +0330

    Update boobs.lua

--- plugins/boobs.lua
+++ plugins/boobs.lua
@@ -70,7 +70,6 @@ return {
 }
 
 end
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+-- مدیر : @mohammadarak
+-- ربات : @avirabot
+-- هر گونه کپی برداری بدون ذکر منبع حرام است 
