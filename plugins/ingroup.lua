f8caadcd5acb39c195ba30834b4ab9cbf273a108
dram1710 <mohammad400ebrahimi@yahoo.com>
2016-03-19 15:39:23 +0330

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -220,7 +220,7 @@ local function show_group_settingsmod(msg, data, target)
     	leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
    	end
   local settings = data[tostring(target)]['settings']
-  local text = "›Group Settings:\n•••Kick new member with link : "..settings.lock_join.."\n•••Lock group Fosh : "..settings.antifosh.."\n•••Lock group chat : "..settings.lock_chat.."\n•••Lock group ads : "..settings.antiads.."\n•••Lock group name : "..settings.lock_name.."\n•••Lock group photo : "..settings.lock_photo.."\n•••kick new member : "..settings.lock_member.."\n•••Lock leave ban : "..leave_ban.."\n•••flood set on : "..NUM_MSG_MAX.."\n•••Bot can come : "..bots_protection.."                                        ›»Avirabot Version 1.2"
+  local text = "›تنظیمات گروه:\n•••حذف  اعضای جدید وارد شده توسط لینک : "..settings.lock_join.."\n•••Lock قفل فحش دادن : "..settings.antifosh.."\n•••قفل چت کردن : "..settings.lock_chat.."\n•••قفل تبلیغات : "..settings.antiads.."\n•••قفل نام گروه : "..settings.lock_name.."\n•••قفل عکس گروه : "..settings.lock_photo.."\n•••حذف اعضای جدید : "..settings.lock_member.."\n•••قفل خروج اعضا : "..leave_ban.."\n•••میزان حساسیت ضد اسپم : "..NUM_MSG_MAX.."\n•••قفل ورود ربات ها : "..bots_protection.."                                        ›»Avirabot Version 1.2"
   return text
 end
 
